fbae069f66d0884a83589729bda02acecfe8cc73
Ethew <118255407+EthanCarollo@users.noreply.github.com>
2023-12-02 10:26:11 +0100

    hotfix: text in project

--- components/ProjectInformation.vue
+++ components/ProjectInformation.vue
@@ -11,7 +11,7 @@ export default {
 <template>
 <div class="shadow-lg shadow-black/30 w-[25vw] h-[20vw] bg-white hover:grow transition-all duration-700 bg-cover bg-center information-project relative"  v-bind:style="{ backgroundImage: 'url('+this.info.image+')' }">
   <div class="absolute top-0 left-0 w-full h-full bg-black/50 hover-information-project flex flex-col justify-center items-center">
-    <h1 class="text-xl text-white w-[60%] text-center">{{info.description}}</h1>
+    <h1 class="text-xl text-white w-[20vw] text-center">{{info.description}}</h1>
   </div>
 </div>
 </template>
