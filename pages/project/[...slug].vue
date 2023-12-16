021f238e9a06add9ba9170cf05bfafad9379c3d7
JackyTuneur <deku.private@gmail.com>
2023-12-16 22:10:36 +0100

    feat(seo): add seo data for ...slug.vue page

--- pages/project/[...slug].vue
+++ pages/project/[...slug].vue
@@ -1,55 +1,50 @@
-<script>
+<script setup>
 import projets from '~/src/data/projects.json';
+const route = useRoute()
 
-export default{
-  layout: 'default',
-  data(){
-    return{
-      projects: projets.data
-    }
-  },
-  computed:{
-    project(){
-      for (let i = 0; i < this.projects.length; i++) {
-        if(this.projects[i].id == this.$route.params.slug){
-          return this.projects[i]
-        }
-      }
-      return null
-    }
-  },
-  created(){
-    if(this.project === null){
-      throw createError({
-        statusCode: 404,
-        statusMessage: 'Page Not Found',
-        fatal:true
-      })
-    }
-  }
+const project = projets.data.find((element) => element.id === route.params.slug[0]);
+
+
+if(project === undefined){
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found',
+    fatal:true
+  })
 }
 
+  useSeoMeta({
+    title: 'Ethan Carollo - ' + project.title,
+    ogTitle: 'Ethan Carollo - ' + project.title,
+    description: project.description,
+    ogDescription: project.description
+  })
+
+
+definePageMeta({
+  layout: 'default'
+})
 </script>
 
 <template>
-  <div v-if="project !== null">
+  <div v-if="project !== undefined">
   <NuxtLink to="/" v-thover="{ scale: 0.5 }" class ="fixed left-8 top-10 w-12 fill-white z-20">
     <IconsLeftIcon></IconsLeftIcon>
   </NuxtLink>
   <div class="min-h-screen flex flex-col items-center" v-thover="{ scale: 1 }">
     <div class="w-full h-[50vh] bg-cover bg-center relative project_card_inner shadow-lg shadow-black/30 flex justify-center items-center"
-         :class="[this.project.id + 'inner']" v-bind:style="{ backgroundImage: 'url('+this.project.image+')' }">
+         :class="[project.id + 'inner']" v-bind:style="{ backgroundImage: 'url('+project.image+')' }">
       <div class="w-full h-full absolute top-0 left-0 bg-black/50"></div>
-      <h1 class="text-white text-6xl z-10 relative">{{this.project.title}}</h1>
+      <h1 class="text-white text-6xl z-10 relative">{{project.title}}</h1>
       <div class ="absolute bottom-0 right-0 w-full flex justify-end p-3 gap-3">
-        <h1 class="bg-white pl-4 pr-4 text-2xl p-1 ">{{ this.project.tag }}</h1>
+        <h1 class="bg-white pl-4 pr-4 text-2xl p-1 ">{{ project.tag }}</h1>
       </div>
       <div class ="absolute bottom-0 left-0 w-full flex p-2 h-16 gap-3 fill-white">
         <Icon v-for="icon in project.icons" :icon="icon"></Icon>
       </div>
     </div>
     <div class="w-[60vw] text-white text-xl mt-8">
-    {{ this.project.description }}
+    {{ project.description }}
     </div>
     <div class = "flex gap-5 w-[90vw] mt-8 flex-wrap">
       <ProjectInformation v-for="info in project.projectinfos.slice(0,3)" :info="info"></ProjectInformation>
@@ -68,7 +63,6 @@ export default{
     </div>
   </div>
 </div>
-
 </template>
 
 <style>
