b2617be214af266a332b2484fab10bf8c9760d12
JackyTuneur <deku.private@gmail.com>
2023-12-18 09:52:56 +0100

    fix(project): fix date of project

--- components/Project.vue
+++ components/Project.vue
@@ -34,13 +34,13 @@ export default{
     :class="[this.project.colSpan, this.project.rowSpan, this.project.id]">
         <div class="w-full h-full bg-cover bg-center project_card_inner shadow-lg shadow-black/30" :class="[this.project.id + 'inner']" v-bind:style="{ backgroundImage: 'url('+this.project.image+')' }">
             <div class="absolute top-0 left-0 w-full h-full bg-black/50 hover_project_class lg:opacity-0 flex flex-col items-center">
-                <h1 class="text-white absolute text-left top-0 left-0 w-[70%] text-lg p-3">{{ this.project.title }}</h1>
-                <h1 class="text-white absolute top-0 right-0 p-3">{{ this.project.date }}</h1>
-                <div class ="absolute bottom-0 left-0 w-full flex p-2 h-12 gap-3 fill-white">
+                <h1 class="text-white absolute text-left top-0 left-0 w-[70%] text-lg lg:text-xl p-3">{{ this.project.title }}</h1>
+                <h1 class="text-white absolute top-0 right-0 lg:text-xl p-3">{{ this.project.date }}</h1>
+                <div class ="absolute bottom-0 left-0 w-full flex p-2 h-12 lg:h-14 gap-3 fill-white">
                     <Icon v-for="icon in project.icons" :icon="icon"></Icon>
                 </div>
-                <div class ="absolute bottom-0 right-0 w-full flex justify-end p-2 h-12 gap-3 fill-white stroke-white">
-                    <h1 class="bg-white pl-4 pr-4 p-1">{{ this.project.tag }}</h1>
+                <div class ="absolute bottom-1 right-0 w-full flex justify-end p-2 h-12 gap-3 fill-white stroke-white">
+                    <h1 class="bg-white pl-4 pr-4 lg:pb-8 lg:text-xl p-1">{{ this.project.tag }}</h1>
                 </div>
             </div>
         </div>
