0f629e9e9739871758fc5ecba8d0342a6b69359f
Ethan Carollo <118255407+EthanCarollo@users.noreply.github.com>
2024-06-01 17:33:40 +0200

    fix(icon): chjange size on icon

--- components/Technologies.vue
+++ components/Technologies.vue
@@ -15,8 +15,8 @@ export default{
         <h1 class ="mt-8 text-2xl text-white mb-4">// technologies</h1>
         <div class="flex lg:w-[70vw] w-[90vw] mt-4 mb-8 lg:gap-6 gap-3 flex-wrap justify-center items-start">
             <div class="lg:w-[8vw] lg:h-[8vw] lg:hover:w-[12.5vw] hover:pb-14 flex justify-center duration-700 transition-all w-[20vw] h-[20vw] lg:p-5 p-3 bg-black/30 rounded-lg fill-white shadow-lg shadow-black/30 relative" v-for="tech in myTech.tech" >
-                <div class = "absolute transition-all bg-black/70 p-3 duration-1000 opacity-0 hover:opacity-100 rounded-lg top-0 left-0 w-full h-full flex justify-center items-end shadow-black/30">
-                    <p class = "text-white lg:text-xl text-lg text-wrap text-center">
+                <div class = "absolute transition-all bg-black/70 p-3 duration-700 opacity-0 hover:opacity-100 lg:hover:text-xl lg:text-sm text-lg rounded-lg top-0 left-0 w-full h-full flex justify-center items-end shadow-black/30">
+                    <p class = "text-white text-wrap text-center">
                         {{  tech  }}
                     </p>
                 </div>
@@ -29,8 +29,8 @@ export default{
         <h1 class ="mt-8 text-2xl text-white mb-4">// logiciels</h1>
         <div class="flex lg:w-[70vw] w-[90vw] mt-4 mb-8 lg:gap-6 gap-3 flex-wrap justify-center items-start">
             <div class="lg:w-[8vw] lg:hover:w-[12.5vw] hover:pb-14 flex justify-center transition-all lg:h-[8vw] duration-700 w-[20vw] relative h-[20vw] lg:p-5 p-3 bg-black/30 rounded-lg fill-white shadow-lg shadow-black/30" v-for="soft in myTech.soft" >
-                <div class = "absolute transition-all bg-black/70 p-3 duration-1000 opacity-0 hover:opacity-100 rounded-lg top-0 left-0 w-full h-full flex justify-center items-end shadow-black/30">
-                    <p class = "text-white lg:text-xl text-lg text-wrap text-center">
+                <div class = "absolute transition-all bg-black/70 p-3 lg:hover:text-xl lg:text-sm text-lg duration-700 opacity-0 hover:opacity-100 rounded-lg top-0 left-0 w-full h-full flex justify-center items-end shadow-black/30">
+                    <p class = "text-white  text-wrap text-center">
                         {{  soft  }}
                     </p>
                 </div>
