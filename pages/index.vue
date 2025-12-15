7c4305c73c7edc33ac97520748fbc412a3baf2b7
Ethan Carollo <etcarollo@gmail.com>
2025-12-15 08:17:27 +0100

    Fix : SEO internationalization

--- pages/index.vue
+++ pages/index.vue
@@ -8,12 +8,17 @@
 </template>
 
 <script setup lang="ts">
+const { t } = useI18n()
+
 useHead({
-    title: 'Ethan Carollo - Polymorphic Developer',
-    meta: [
-        { name: 'description', content: 'Ethan Carollo, a polymorphic developer skilled in web and mobile applications, video games, and continuous delivery cycles.' },
-        { name: 'keywords', content: 'Ethan Carollo, developer, web applications, mobile applications, video games, continuous delivery, Kotlin, Python, Nuxt, Unity' },
-        { name: 'author', content: 'Ethan Carollo' }
-    ]
+    titleTemplate: '%s' // Prevent appending "| Site Name"
+})
+
+useSeoMeta({
+    title: () => t('seo.title'),
+    description: () => t('seo.description'),
+    ogTitle: () => t('seo.title'),
+    ogDescription: () => t('seo.description'),
+    twitterCard: 'summary_large_image',
 })
 </script>
